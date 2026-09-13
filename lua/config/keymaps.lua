a8497ca231fad6d634120636e81b9c6a4027bc49
kineticacapella <1siyath1025@gmail.com>
2026-09-14 00:49:26 +0100

    Removed duplicate Telescope leader bind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,10 +13,9 @@ map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
--- Telescope
+-- Telescope (a bit like helix)
 map("n", "<leader>f", function() require("telescope.builtin").find_files() end, { desc = "Find files" })
 map("n", "<leader>F", function() require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() }) end, { desc = "Find files (cwd)" })
-map("n", "<leader>e", function() require("telescope.builtin").find_files() end, { desc = "File explorer" })
 map("n", "<leader>b", function() require("telescope.builtin").buffers() end, { desc = "Buffers" })
 map("n", "<leader>g", function() require("telescope.builtin").live_grep() end, { desc = "Live grep" })
 map("n", "<leader>h", function() require("telescope.builtin").help_tags() end, { desc = "Help tags" })
