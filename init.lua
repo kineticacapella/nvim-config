6595c3b514dfdacf4628b20a596c5a9284dddb22
kineticacapella <1siyath1025@gmail.com>
2026-09-13 22:11:07 +0100

    make StatusLine bg transparent

--- init.lua
+++ init.lua
@@ -1,9 +1,20 @@
 require("config.options")
 require("config.keymaps")
+
+-- Set StatusLine background to transparent before requiring lazy
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+  end,
+})
+
+-- Now load lazy
 require("config.lazy")
 
 -- fixing warnings
-
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+
